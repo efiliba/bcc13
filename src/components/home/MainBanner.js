8a22e1d30922302f80d5c8f2aeacc71fad0c01ed
Eli Filiba <efiliba@hotmail.com>
2023-06-16 10:05:59 +1000

    Add responsive toolbar

--- src/components/home/MainBanner.js
+++ src/components/home/MainBanner.js
@@ -5,7 +5,7 @@ import { Button } from '@/components';
 
 export const MainBanner = ({ image, title, description, action }) =>
   <div className="relative min-h-[700px] grid items-center justify-items-center">
-    <Image className="object-cover" fill src={image} alt={description} />
+    <Image className="object-cover" fill src={image} alt={description} priority/>
     <div className="absolute text-white text-center md:text-left px-3 w-full max-w-screen-xl mx-auto">
       <h1 className="text-6xl">{title}</h1>
       <h2 className="mt-4 text-xl max-w-[550px]">{description}</h2>
@@ -14,66 +14,3 @@ export const MainBanner = ({ image, title, description, action }) =>
       </Link>
     </div>
   </div>;
-
-{/* <div className={`px-3 ${className}`}>
-<div className="max-w-screen-xl mx-auto"> */}
-
-// .main-banner {
-//   position: relative;
-//   background-color: slategrey;
-
-//   &__content {
-//     position: absolute;
-//     width: 100%;
-//     top: 50%;
-//     transform: translateY(-50%);
-//     color: white;
-    
-//     &__container {
-//       // @include content-layout();
-//       max-width: map-get($breakpoints, 'lg');
-//       margin: auto;
-//       padding: 0 1rem;
-//       text-align: center;
-
-//       @include breakpoint(sm) {
-//         text-align: left;
-//       }
-
-//       .title {
-//         font-size: 3.5rem;
-//         line-height: 4.2rem;
-//         font-weight: normal;
-//         padding: 0 2rem;
-
-//         @include breakpoint(sm) {
-//           font-size: 6.5rem;
-//           line-height: 7.8rem;
-//           padding: 0;
-//           max-width: 84.4rem;
-//         }
-//       }
-
-//       .description {
-//         font-size: 1.6rem;
-//         line-height: 2.4rem;
-//         font-weight: normal;
-//         margin-bottom: 6rem;
-//         padding: 0 1rem;
-
-//         @include breakpoint(sm) {
-//           font-size: 2.2rem;
-//           line-height: 2.5rem;
-//           padding: 0;
-//           max-width: 57.2rem;
-//         }
-//       }
-
-//       a {
-//         @include responsive-button-size();
-
-//         color: $white;
-//       }
-//     }
-//   }
-// }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,8 @@
 "use client";
 
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-// import { TabsUnstyled, TabsListUnstyled, TabUnstyled } from '@mui/base';
-// import { Toolbar, Icon, Typography } from '@mui/material';
-// import classNames from 'classnames';
+import Image from 'next/image';
 
 import { pageLinks } from '@/services';
 
@@ -29,7 +26,7 @@ import { pageLinks } from '@/services';
 //   </Toolbar>;
 
 export const Header = () => {
-  const router = useRouter();
+  const [show, setShow] = useState(false);
 
   // const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
@@ -37,52 +34,48 @@ export const Header = () => {
   //   setSelectedTabIndex(pageLinks.findIndex(({ to }) => to === router.pathname));
   // }, [router.pathname]);
 
-  const handleRouteClick = (url) => () => router.push(url);
+  const handleHamburgerClick = () => setShow(state => !state);
 
   return (
     <header className="header">
       {/* <div className="header-strip">
         <HeaderStrip />
       </div> */}
-      <div className="header__content">
-        <Link href="/" passHref>
-          <img src="/images/best-choice-care.png" alt="Best Choice Care" />
+      <div className="p-4 bg-white grid grid-cols-[194px_1fr_min-content]">
+        <Link className="relative h-20" href="/" passHref>
+          <Image
+            src="/images/best-choice-care.png"
+            alt="Best Choice Care"
+            fill
+            priority
+          />
         </Link>
-        {pageLinks.map(({ to, text }, index) =>
-          <div
-            key={index}
-            // className={classNames({ selected: index === selectedTabIndex })}
-            onClick={handleRouteClick(to)}
-          >
-            <div>{text}</div>
-          </div>
-        )}
-        <div className="icon-text__phone-link">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+        <button
+          type="button"
+          className="md:hidden col-start-3 text-primary hover:text-secondary focus:text-secondary focus:outline-none"
+          onClick={handleHamburgerClick}
+        >
+          <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+            {show
+              ? <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              : <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            }
           </svg>
-          <div variant="subtitle2">Call Us</div>
-          <div variant="h4">1300 711 380</div>
+        </button>
+        <div className={`${show ? 'grid' : 'hidden'} md:grid col-start-2 col-span-2 justify-self-end md:grid-flow-col items-baseline lg:items-center`}>
+          {pageLinks.map(({ to, text }, index) =>
+            <Link key={index} className="md:text-xs lg:text-base font-semibold text-gray-500 hover:bg-primary hover:text-white rounded p-2" href={to}>
+              {text}
+            </Link>
+          )}
+          <div className="p-2 text-secondary grid grid-cols-[max-content_max-content] items-center gap-x-5 gap-y-1">
+            <svg className="w-6 h-6 row-span-2 md:row-span-1 lg:row-span-2 fill-secondary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+            </svg>
+            <div className="text-xs">Call Us</div>
+            <div className="md:col-span-2 lg:col-span-1 text-lg md:text-sm lg:text-lg font-bold">1300 711 380</div>
+          </div>
         </div>
-
-      {/*   <TabsUnstyled className="navigation">
-          <TabsListUnstyled className="navigation__list">
-            {pageLinks.map(({ to, text }, index) =>
-              <TabUnstyled
-                key={index}
-                className={classNames({ selected: index === selectedTabIndex })}
-                onClick={handleRouteClick(to)}
-              >
-                <Typography variant="body2">{text}</Typography>
-              </TabUnstyled>
-            )}
-          </TabsListUnstyled>
-        </TabsUnstyled>
-        <div className="icon-text__phone-link">
-          <Icon className="fa fa-phone-alt" />
-          <Typography variant="subtitle2">Call Us</Typography>
-          <Typography variant="h4">1300 711 380</Typography>
-        </div> */}
       </div>
     </header>
   );
