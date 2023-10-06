a340173b37bffe27c78247d721c95845c138b15b
Eli Filiba <efiliba@hotmail.com>
2023-10-06 11:22:30 +0300

    Fix build issues

--- src/components/home/LiveAtHome.tsx
+++ src/components/home/LiveAtHome.tsx
@@ -8,7 +8,7 @@ export const LiveAtHome = () =>
       <h1 className="font-bold text-2xl md:text-4xl pb-5">Already have an NDIS plan?</h1>
       <p>
         Fantastic! Reach out to us directly and we can organise services immediately. If you have a support
-        coordinator, we're happy to speak with them on your behalf. Get started here or call 1300 711 380.
+        coordinator, we&apos;re happy to speak with them on your behalf. Get started here or call 1300 711 380.
       </p>
     </SplitImageContent>
     <Image
--- src/components/home/Why.tsx
+++ src/components/home/Why.tsx
@@ -11,11 +11,11 @@ export const Why = ({ tiles }: Props) =>
       In-Home Care Offers A Dignifying, Reliable, And Comfortable Care Experience
     </h4>
     <p className="mt-4 sm:px-[min(6.25rem,8%)]">
-      Home is more than just a location; it's a cherished sentiment. Indeed, it's the center of our lives. Our
+      Home is more than just a location; it&apos;s a cherished sentiment. Indeed, it&apos;s the center of our lives. Our
       dedicated support team is always on standby, ensuring that even the most challenging tasks become manageable.
       With our in-home care, you can maintain your independence, surrounded by the familiar comforts of your space.
-      It's tailored to your preferences – you have the autonomy to choose how you want your care delivered. Remember,
-      it's your home, your journey, and we're here to support you every step of the way.
+      It&apos;s tailored to your preferences – you have the autonomy to choose how you want your care delivered. Remember,
+      it&apos;s your home, your journey, and we&apos;re here to support you every step of the way.
     </p>
     <Tiles tiles={tiles} />
   </Container>;
